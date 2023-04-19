d1054f51470b30403a7d0628000dd01df1e168c9
Paula Leal <paula61395@gmail.com>
2023-04-19 15:57:38 -0400

    fixed pagination

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -1,25 +1,18 @@
 // Pagination Code from Traversy Media the YouTube video is called Simple Frontend Pagination | React
 
 import Page from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
 
 const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
     const pageNumbers = [];
+
+
 
     for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
         pageNumbers.push(i);
     }
 
     return (
-        <nav className='paginationContainer'>
-            {pageNumbers.map(number => (
-                <Stack key={number} className='page-item'>
-                    <Page onClick={() => paginate(number)} href='!#' count={number} color="warning" variant="outlined">
-                        {number}
-                    </Page>
-                </Stack>
-            ))}
-        </nav>
+        <Page onChange={(event, value) => paginate(value)} count={pageNumbers.length} color="warning" variant="outlined" />
     );
 };
 
