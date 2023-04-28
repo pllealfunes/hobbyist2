bc90e3d8053ebbc96b5d3e5dfb3328c71d1cbc09
Paula Leal <paula61395@gmail.com>
2023-04-27 23:05:50 -0400

    changed cors in users and blogAPI

--- server/api/users.js
+++ server/api/users.js
@@ -15,14 +15,20 @@ const verifyJWT = require('../middleware/verifyJWT')
 
 /* CORS */
 
-const corsOptions = {
-    origin: `${process.env.FRONTEND}`,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-}
-router.use(cors(corsOptions))
+router.use((req, res, next) => {
+    res.set({
+        // allow any domain, allow REST methods we've implemented
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+        "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers",
+        // Set content-type for all api requests
+        'Content-type': 'application/json'
+    });
+    if (req.method == 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
 
 
 
