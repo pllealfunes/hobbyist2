11ad78d72c22c85a8873df6c10823f084a80c25a
Paula Leal <paula61395@gmail.com>
2023-04-27 20:15:55 -0400

    edited not Found component

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,11 +1,36 @@
 import { Link } from 'react-router-dom'
 
+import {
+    Card,
+    CardContent,
+    CardMedia,
+    Typography,
+    Grid,
+    Box
+
+} from "@mui/material";
+
 const NotFound = () => {
 
     return (
         <div className="container">
-            <p>I'm sorry but the page you are lookking for can't be found. Click the link below to return to the homepage</p>
-            <div><Link to="/">Home</Link></div>
+            <section className='emptyPostsWrapper'>
+                <Grid
+                    container
+                    className="emptyPostsContainer"
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    sx={{
+                        boxShadow: 2,
+                        '& button': { my: 3 },
+                    }}
+                    width={700}
+                >
+                    <Typography variant="h5">OOPS! Something Went Wrong</Typography>
+                    <Typography variant="subtitle1">I'm sorry but the page you are looking for can't be found</Typography>
+                </Grid>
+            </section>
         </div>
     )
 }
