7429a1e1e09ad5c5f460a733753dd068084f9b7c
Paula Leal <paula61395@gmail.com>
2023-05-04 22:25:59 -0400

    changed follow category button

--- client/src/components/FollowCategoryBtn.js
+++ client/src/components/FollowCategoryBtn.js
@@ -7,97 +7,83 @@ import { toast } from 'react-toastify'
 import axiosPrivate from '../config/useAxiosPrivate'
 
 function FollowCategoryBtn({ category, user }) {
-
-    const [currentUser, setCurrentUser] = useState('')
-    const [isFollow, setIsFollow] = useState(false)
-    const [isLoading, setIsLoading] = useState(true);
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-
+    const [currentUser, setCurrentUser] = useState('');
+    const [isFollow, setIsFollow] = useState(false);
+    const [initialIsFollow, setInitialIsFollow] = useState(false); // new state variable
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         (async () => {
             try {
                 if (user) {
-                    let response = await dispatch(getUser(user.currentUser.id))
-                    setCurrentUser(response.payload)
+                    let response = await dispatch(getUser(user.currentUser.id));
+                    setCurrentUser(response.payload);
                 }
-
             } catch (error) {
                 console.log(error);
             }
-
         })();
-
-    }, [user, dispatch])
+    }, [user, dispatch]);
 
     useEffect(() => {
         (async () => {
             try {
                 if (currentUser) {
-                    setIsLoading(false);
                     if (currentUser.categories.includes(category)) {
                         setIsFollow(true);
+                        setInitialIsFollow(true); // update initial value
                     } else {
                         setIsFollow(false);
+                        setInitialIsFollow(false); // update initial value
                     }
                 }
             } catch (error) {
                 console.log(error);
             }
         })();
-    }, [currentUser]);
+    }, [currentUser, category]);
 
     const followCategory = async () => {
         try {
-            await axiosPrivate.put(`/users/${user.currentUser.id}/followCategory/${category}`)
-            toast.success(`Now following ${category}`)
-            setIsFollow(true)
+            await axiosPrivate.put(`/users/${user.currentUser.id}/followCategory/${category}`);
+            toast.success(`Now following ${category}`);
+            setIsFollow(true);
         } catch (error) {
-            toast.error("Try again later")
+            toast.error('Try again later');
         }
-
-    }
+    };
 
     const unfollowCategory = async () => {
         try {
-            await axiosPrivate.put(`/users/${user.currentUser.id}/unFollowCategory/${category}`)
-            toast.success(`Now unfollowing ${category}`)
-            setIsFollow(false)
+            await axiosPrivate.put(`/users/${user.currentUser.id}/unFollowCategory/${category}`);
+            toast.success(`Now unfollowing ${category}`);
+            setIsFollow(false);
         } catch (error) {
-            toast.error("Try again later")
+            toast.error('Try again later');
         }
-    }
+    };
 
     return (
         <div>
-            <div>
-                {
-                    user ?
-                        <button
-                            className="followBtn"
-                            onClick={() => {
-                                if (isLoading) return; // disable button while loading
-                                if (isFollow) {
-                                    unfollowCategory();
-                                } else {
-                                    followCategory();
-                                }
-                            }}
-                            disabled={isLoading} // disable button while loading
-                        >
-                            {isFollow ? `Unfollow ${category}` : `Follow ${category}`}
-                        </button>
-                        :
-                        <button onClick={() => (navigate('/login'))}>
-                            Follow {category}
-                        </button>
-                }
-            </div>
+            {user ? (
+                <button
+                    className="followBtn"
+                    onClick={() => {
+                        if (isFollow) {
+                            unfollowCategory();
+                        } else {
+                            followCategory();
+                        }
+                    }}
+                >
+                    {isFollow ? `Unfollow ${category}` : `Follow ${category}`}
+                </button>
+            ) : (
+                <div>Category: {category}</div>
+            )}
         </div>
-
-
-    )
+    );
 }
 
 export default FollowCategoryBtn
